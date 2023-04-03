f80467bbc1746477fcc29f4c8e800c785050d601
Ryan Swift <ryanswift93@gmail.com>
2023-04-03 15:10:30 -0400

    Adding hackathons route and date

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+hackathons = {
+    "GHW: API Week": {
+        "start_date": "2023-04-03 12:00:00",
+        "end_date": "2023-04-10 12:00:00",
+        "location": "Everywhere, Online",
+        "type": "Digital Only"
+    },
+    "Bitcamp": {
+        "start_date": "2023-04-07 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
+        "location": "College Park, Maryland",
+        "type": "In-Person Only"
+    }
+}
 
 @app.route("/")
 def hello_ghw():
 	return "<p>Thanks, 404, for all the help!!!</p>"
+
+@app.route('/getHackathons', methods=['GET'])
+def getHackathons():
+	return hackathons
 
 if __name__=="__main__":
 	app.run(debug = True)
