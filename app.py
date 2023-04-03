9b9ce7c20e2522118dece88e6b10939143518e6a
Ryan Swift <ryanswift93@gmail.com>
2023-04-03 15:03:11 -0400

    Thanking 404

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_ghw():
-	return "<p>Hello, yeskaydee!</p>"
+	return "<p>Thanks, 404, for all the help!!!</p>"
 
 if __name__=="__main__":
 	app.run(debug = True)
